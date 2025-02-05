856035fd73e56436cdf3bf972dc7f6a033d2e17a
murry2018 <2clean8@naver.com>
2025-02-05 16:24:52 +0900

    Remove the quotes when calling `init-for'

--- lisp/pref-posframe.el
+++ lisp/pref-posframe.el
@@ -1,12 +1,12 @@
 (require 'pref-packages)
 
-(init-for 'ivy-posframe
+(init-for ivy-posframe
   (ivy-posframe-mode 1))
-(init-for 'which-key-posframe
+(init-for which-key-posframe
   (which-key-posframe-mode 1))
-(init-for 'eldoc-box
+(init-for eldoc-box
   (add-hook 'eglot-managed-mode-hook #'eldoc-box-hover-mode t))
-(init-for 'flycheck-posframe
+(init-for flycheck-posframe
   (with-eval-after-load 'flycheck
     (require 'flycheck-posframe)
     (add-hook 'flycheck-mode-hook #'flycheck-popup-tip-mode)))
